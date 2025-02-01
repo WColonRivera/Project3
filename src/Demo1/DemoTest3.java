7e7a608926798458e0e2238621a7d65b418f7106
17874 <17874@DESKTOP-G3LKH5V.lan>
2025-01-31 22:00:17 -0500

    Implemented new ()

--- src/Demo1/DemoTest3.java
+++ src/Demo1/DemoTest3.java
@@ -3,7 +3,12 @@ package Demo1;
 public class DemoTest3 {
 	
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("Batch13");
+		
+		int a= 10;
+		int b = 30;
+		
+		System.out.println(a+b);
 		
 	}
 
